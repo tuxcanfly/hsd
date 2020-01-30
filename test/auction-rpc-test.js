b6621cfcb8a37b4b65f1082820682eb2281c31a7
Javed Khan <tuxcanfly@gmail.com>
2020-01-30 14:37:16 +0530

    test: increase timeouts

--- test/auction-rpc-test.js
+++ test/auction-rpc-test.js
@@ -42,7 +42,7 @@ class TestUtil {
     this.node.use(plugin);
 
     this.nclient = new NodeClient({
-      timeout: 10000,
+      timeout: 15000,
       host: options.host,
       port: options.nport
     });
